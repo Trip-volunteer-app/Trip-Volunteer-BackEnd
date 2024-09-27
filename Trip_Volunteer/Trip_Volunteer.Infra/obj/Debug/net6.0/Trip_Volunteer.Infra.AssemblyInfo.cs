c394afe840de4fbc10ebbe4a23956ce5dd8e96fa
sajedaquraan <sajedaalquraan1@gmail.com>
2024-09-27 21:02:56 +0300

    hadeel

--- Trip_Volunteer/Trip_Volunteer.Infra/obj/Debug/net6.0/Trip_Volunteer.Infra.AssemblyInfo.cs
+++ Trip_Volunteer/Trip_Volunteer.Infra/obj/Debug/net6.0/Trip_Volunteer.Infra.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trip_Volunteer.Infra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8af05d614b0f7c23d987ee75dc35c00ea0bf13b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+235b396d27170e2b3e12b99a9ca8743c186d174d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trip_Volunteer.Infra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trip_Volunteer.Infra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
