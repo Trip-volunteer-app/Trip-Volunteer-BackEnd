998f59fcf2c41b500a2b66e588fd12b5be397dd8
Amani Alsmadi <smadi.amani94@gmail.com>
2024-09-28 16:23:14 +0300

    main

--- Trip_Volunteer/Trip_Volunteer.Core/obj/Debug/net6.0/Trip_Volunteer.Core.AssemblyInfo.cs
+++ Trip_Volunteer/Trip_Volunteer.Core/obj/Debug/net6.0/Trip_Volunteer.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trip_Volunteer.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cba1e29c1d56d5fdaca121260ef4e946bae575d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa3f7ab89aa5337fee273eca4b907fb5be3f0fb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trip_Volunteer.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trip_Volunteer.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
