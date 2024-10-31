26c4242af601d6385976a9d706b83137f35664f0
sajedaquraan <sajedaalquraan1@gmail.com>
2024-10-31 19:13:15 +0300

    Add Card Table

---
+++ Trip_Volunteer/Trip_Volunteer.Core/Data/BookingService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Trip_Volunteer.Core.Data
+{
+    public partial class BookingService
+    {
+        public decimal BookingServiceId { get; set; }
+        public decimal? BookingId { get; set; }
+        public decimal? ServiceId { get; set; }
+
+        public virtual Booking? Booking { get; set; }
+        public virtual Service? Service { get; set; }
+    }
+}
---
+++ Trip_Volunteer/Trip_Volunteer.Core/Data/Card.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Trip_Volunteer.Core.Data
+{
+    public partial class Card
+    {
+        public decimal CardId { get; set; }
+        public string? CardholderName { get; set; }
+        public decimal? CardNumber { get; set; }
+        public DateTime? ExpiryDate { get; set; }
+        public decimal? LoginId { get; set; }
+
+        public virtual UserLogin? Login { get; set; }
+    }
+}
