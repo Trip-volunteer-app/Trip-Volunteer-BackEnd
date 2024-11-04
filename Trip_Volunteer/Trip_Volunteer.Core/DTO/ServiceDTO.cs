8e86be8d72be62ae8ae2e3a8868693a4934081ff
Hadeel-Ali-Obaid <hadeelobaid71@gmail.com>
2024-11-04 15:03:29 +0200

    trip and service dto

---
+++ Trip_Volunteer/Trip_Volunteer.Core/DTO/ServiceDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trip_Volunteer.Core.DTO
+{
+    public class ServiceDTO
+    {
+        public decimal? Trip_Id { get; set; }
+        public string? Trip_Name { get; set; }
+        public decimal? Service_Id { get; set; }
+        public string? Service_Name { get; set; }
+        public decimal? Service_Cost { get; set; }
+    }
+}
