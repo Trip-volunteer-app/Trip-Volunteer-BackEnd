f6abb268d712d6c7e00d5a48880cbc65fa98b435
sajedaquraan <sajedaalquraan1@gmail.com>
2024-11-06 13:35:44 +0300

    update DTO for Reports

--- Trip_Volunteer/Trip_Volunteer.Core/DTO/MonthlyReportDTO.cs
+++ Trip_Volunteer/Trip_Volunteer.Core/DTO/MonthlyReportDTO.cs
@@ -9,7 +9,11 @@ namespace Trip_Volunteer.Core.DTO
     public class MonthlyReportDTO
     {
         public DateTime trip_month { get; set; }
-        public decimal MONTHLY_REVENUE { get; set; }
+        public decimal TOTAL_SERVICE_COST { get; set; }
+        public decimal TOTAL_REVENUE { get; set; }
+
+        public decimal NET_REVENUE { get; set; }
+
     }
 
 }
